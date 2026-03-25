4f0eada97e2cdbe53aea6fe58ac779fe2830d2fa
guliangsi <43655090+guliangsi@users.noreply.github.com>
2026-03-25 13:39:46 +0100

    cccvdfr

--- custom_components/freebox_parental/config_flow.py
+++ custom_components/freebox_parental/config_flow.py
@@ -1,9 +1,6 @@
-import asyncio
 import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST
-from homeassistant.data_entry_flow import FlowResult
 
 from .freebox_api import FreeboxAPI
 
@@ -11,81 +8,35 @@ DOMAIN = "freebox_parental"
 
 
 class FreeboxFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
 
-    def __init__(self):
-        self._host = None
-        self._api = None
-        self._track_id = None
-        self._app_token = None
-
-    async def async_step_user(self, user_input=None) -> FlowResult:
+    async def async_step_user(self, user_input=None):
         if user_input is not None:
-            self._host = user_input[CONF_HOST]
-            self._api = FreeboxAPI(self._host)
+            host = user_input[CONF_HOST]
+
+            api = FreeboxAPI(host)
 
             try:
-                # Lancer autorisation
-                res = await self._api.authorize()
+                # 🔥 magie: la lib gère l’auth complète
+                await api.authorize()
 
-                self._track_id = res["track_id"]
-                self._app_token = res["app_token"]
-
-                return self.async_show_progress(
-                    step_id="authorize",
-                    progress_action="waiting_for_user",
-                )
-
-            except Exception as e:
+            except Exception:
                 return self.async_show_form(
                     step_id="user",
-                    data_schema=self._get_schema(),
+                    data_schema=self._schema(),
                     errors={"base": "cannot_connect"},
                 )
 
+            return self.async_create_entry(
+                title="Freebox Parental",
+                data={CONF_HOST: host},
+            )
+
         return self.async_show_form(
             step_id="user",
-            data_schema=self._get_schema(),
+            data_schema=self._schema(),
         )
 
-    async def async_step_authorize(self, user_input=None) -> FlowResult:
-        """Attente validation sur Freebox"""
-
-        while True:
-            status = await self._api.check_authorization(self._track_id)
-
-            if status == "granted":
-                break
-
-            if status == "denied":
-                return self.async_abort(reason="access_denied")
-
-            await asyncio.sleep(2)
-
-        return self.async_show_progress_done(
-            next_step_id="finish"
-        )
-
-    async def async_step_finish(self, user_input=None) -> FlowResult:
-        """Login + création entrée"""
-
-        # injecter le token dans l'API
-        self._api._app_token = self._app_token
-
-        try:
-            await self._api.login()
-        except Exception:
-            return self.async_abort(reason="cannot_connect")
-
-        return self.async_create_entry(
-            title="Freebox Parental",
-            data={
-                CONF_HOST: self._host,
-                "app_token": self._app_token,
-            },
-        )
-
-    def _get_schema(self):
+    def _schema(self):
         return vol.Schema({
             vol.Required(CONF_HOST, default="mafreebox.freebox.fr"): str,
         })
--- custom_components/freebox_parental/freebox_api.py
+++ custom_components/freebox_parental/freebox_api.py
@@ -1,160 +1,26 @@
-import aiohttp
-import asyncio
-import hashlib
-import logging
-
-_LOGGER = logging.getLogger(__name__)
-
+from aiofreepybox import Freepybox
 
 class FreeboxAPI:
     def __init__(self, host):
-        self._host = host
-        self._base = f"http://{host}/api/v4"
-        self._session_token = None
-        self._app_token = None
+        self._fbx = Freepybox(host)
 
-        self._app_id = "hass.freebox_parental"
-        self._app_name = "Home Assistant Freebox Parental"
-        self._app_version = "1.0"
-        self._device_name = "Home Assistant"
-
-        self._http = aiohttp.ClientSession()
-
-    # ---------------------------
-    # HTTP CORE
-    # ---------------------------
-    async def _request(self, method, path, data=None):
-        url = f"{self._base}{path}"
-
-        headers = {}
-        if self._session_token:
-            headers["X-Fbx-App-Auth"] = self._session_token
-
-        try:
-            async with self._http.request(
-                method,
-                url,
-                json=data,
-                headers=headers,
-                timeout=aiohttp.ClientTimeout(total=10),
-            ) as resp:
-                res = await resp.json()
-
-        except asyncio.TimeoutError:
-            raise Exception("Timeout Freebox API")
-
-        except aiohttp.ClientError as err:
-            raise Exception(f"HTTP error: {err}")
-
-        if not res.get("success", False):
-            _LOGGER.debug("Freebox API error: %s", res)
-            raise Exception(res)
-
-        return res
-
-    async def close(self):
-        await self._http.close()
-
-    # ---------------------------
-    # AUTHORIZE (1 seule fois)
-    # ---------------------------
     async def authorize(self):
-        res = await self._request("POST", "/login/authorize/", {
-            "app_id": self._app_id,
-            "app_name": self._app_name,
-            "app_version": self._app_version,
-            "device_name": self._device_name,
-        })
+        return await self._fbx.open()
 
-        return {
-            "track_id": res["result"]["track_id"],
-            "app_token": res["result"]["app_token"],
-        }
-
-    async def check_authorization(self, track_id):
-        res = await self._request("GET", f"/login/authorize/{track_id}")
-        return res["result"]["status"]
-
-    # ---------------------------
-    # LOGIN (session)
-    # ---------------------------
     async def login(self):
-        if not self._app_token:
-            raise Exception("Missing app_token")
+        await self._fbx.open()
 
-        # 1. récupérer challenge
-        res = await self._request("GET", "/login/")
-        challenge = res["result"]["challenge"]
-
-        # 2. générer password
-        password = hashlib.sha1(
-            (challenge + self._app_token).encode()
-        ).hexdigest()
-
-        # 3. ouvrir session
-        res = await self._request("POST", "/login/session/", {
-            "app_id": self._app_id,
-            "password": password,
-        })
-
-        self._session_token = res["result"]["session_token"]
-
-    # ---------------------------
-    # UTILS
-    # ---------------------------
-    async def ensure_logged_in(self):
-        if not self._session_token:
-            await self.login()
-
-    async def _safe_request(self, method, path, data=None):
-        try:
-            await self.ensure_logged_in()
-            return await self._request(method, path, data)
-
-        except Exception:
-            _LOGGER.debug("Session expirée, relogin…")
-            self._session_token = None
-            await self.login()
-            return await self._request(method, path, data)
-
-    # ---------------------------
-    # PARENTAL CONTROL
-    # ---------------------------
     async def get_profiles(self):
-        res = await self._safe_request("GET", "/parental/filter/")
-        return res["result"]
+        return await self._fbx.parental.get_filters()
 
     async def pause_profile(self, profile_id):
-        return await self._safe_request(
-            "PUT",
-            f"/parental/filter/{profile_id}",
-            {
-                "forced": True,
-                "forced_mode": "denied",
-            },
+        return await self._fbx.parental.set_filter(
+            profile_id,
+            {"forced": True, "forced_mode": "denied"}
         )
 
     async def resume_profile(self, profile_id):
-        return await self._safe_request(
-            "PUT",
-            f"/parental/filter/{profile_id}",
-            {
-                "forced": False,
-            },
-        )
-
-    async def set_profile_mode(self, profile_id, mode):
-        """
-        mode:
-        - allowed
-        - denied
-        - webonly
-        """
-        return await self._safe_request(
-            "PUT",
-            f"/parental/filter/{profile_id}",
-            {
-                "forced": True,
-                "forced_mode": mode,
-            },
+        return await self._fbx.parental.set_filter(
+            profile_id,
+            {"forced": False}
         )
